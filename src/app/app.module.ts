f1cb6159f566750816b5f7e3eef13e0353107252
fedorenko <fedorenko@reshenie-soft.ru>
2022-12-29 18:05:47 +0300

    8. Settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SettingsComponent } from './components/settings/settings.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { EventComponent } from './components/event/event.component';
 import { CategoryComponent } from './components/category/category.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +20,15 @@ import { CategoryComponent } from './components/category/category.component';
     SettingsComponent,
     SummaryComponent,
     EventComponent,
-    CategoryComponent
+    CategoryComponent,
+    SettingsPageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

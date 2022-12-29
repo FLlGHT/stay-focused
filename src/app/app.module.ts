48d0983e95c151ae1d9d9c82a3372fbceb7f7e52
fedorenko <fedorenko@reshenie-soft.ru>
2022-12-29 12:56:51 +0300

    6. Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomePageComponent
+    HomePageComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,

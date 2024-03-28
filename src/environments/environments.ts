fa7cf61072a57178019ce8397c6e0cc221594bbe
Michael Fedorenko <mwxj@mail.ru>
2024-03-28 07:55:15 +0300

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,3 @@
 export const environment = {
-  CLIENT_ID: '74526733780-olcqqqepah4b98m2ehqsf9tim3p7l1tu.apps.googleusercontent.com',
-  API_KEY: 'AIzaSyAxpo3iI7siUajbBa6YZ66y7QsM_s3VpI8',
-  DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-  SCOPE: 'https://www.googleapis.com/auth/calendar.readonly'
+  // not relevant
 }
